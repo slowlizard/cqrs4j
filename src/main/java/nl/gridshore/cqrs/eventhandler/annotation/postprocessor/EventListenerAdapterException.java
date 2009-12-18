21b9131b62e3f22988271a212847986f2290018c
buijze <buijze@e329d720-ea76-11de-8016-dd4ad4b6fd10>
2009-12-18 07:14:58 +0000

    - minor refactoring - improved documentation

--- src/main/java/nl/gridshore/cqrs/eventhandler/annotation/postprocessor/EventListenerAdapterException.java
+++ src/main/java/nl/gridshore/cqrs/eventhandler/annotation/postprocessor/EventListenerAdapterException.java
@@ -3,9 +3,11 @@ package nl.gridshore.cqrs.eventhandler.annotation.postprocessor;
 import org.springframework.beans.BeansException;
 
 /**
+ * Exception indicating that an error occurred while creating an EventListenerAdapter for an event listener
+ *
  * @author Allard Buijze
  */
-class EventListenerAdapterException extends BeansException {
+public class EventListenerAdapterException extends BeansException {
 
     public EventListenerAdapterException(String message, Throwable cause) {
         super(message, cause);
