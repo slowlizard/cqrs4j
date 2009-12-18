8e4102e3702cc8509a3409798d404c09375b066c
buijze <buijze@e329d720-ea76-11de-8016-dd4ad4b6fd10>
2009-12-18 08:42:02 +0000

    Major package renaming to be consistent with the groupId and artifactId of the project

---
+++ trunk/src/main/java/nl/gridshore/cqrs4j/eventhandler/annotation/postprocessor/EventListenerAdapterException.java
@@ -0,0 +1,15 @@
+package nl.gridshore.cqrs4j.eventhandler.annotation.postprocessor;
+
+import org.springframework.beans.BeansException;
+
+/**
+ * Exception indicating that an error occurred while creating an EventListenerAdapter for an event listener
+ *
+ * @author Allard Buijze
+ */
+public class EventListenerAdapterException extends BeansException {
+
+    public EventListenerAdapterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
