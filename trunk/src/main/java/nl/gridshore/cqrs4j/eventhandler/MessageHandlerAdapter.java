e8d47da2e6586fc845c158cfa2ccfa79054ac719
buijze <buijze@e329d720-ea76-11de-8016-dd4ad4b6fd10>
2010-01-05 12:24:25 +0000

    Created Handleradapter test (and minor modification to filter events using "canHandle")

--- trunk/src/main/java/nl/gridshore/cqrs4j/eventhandler/MessageHandlerAdapter.java
+++ trunk/src/main/java/nl/gridshore/cqrs4j/eventhandler/MessageHandlerAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009. Gridshore
+ * Copyright (c) 2010. Gridshore
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,6 +47,9 @@ class MessageHandlerAdapter implements MessageHandler {
      */
     @Override
     public void handleMessage(Message<?> message) throws MessageHandlingException, MessageDeliveryException {
-        eventListener.handle((DomainEvent) message.getPayload());
+        DomainEvent event = (DomainEvent) message.getPayload();
+        if (eventListener.canHandle(event.getClass())) {
+            eventListener.handle(event);
+        }
     }
 }
