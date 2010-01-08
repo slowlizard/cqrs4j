aaf6c5c781dc674b0c29a13cd598c82f6a084683
buijze <buijze@e329d720-ea76-11de-8016-dd4ad4b6fd10>
2010-01-08 07:45:22 +0000

    Removed the event handler cache. It is an example of premature optimization, as a full method scan is (slightly) faster than a cache lookup.

--- src/test/java/nl/gridshore/cqrs4j/eventhandler/annotation/AnnotationEventHandlerInvokerTest.java
+++ src/test/java/nl/gridshore/cqrs4j/eventhandler/annotation/AnnotationEventHandlerInvokerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009. Gridshore
+ * Copyright (c) 2010. Gridshore
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,7 +79,7 @@ public class AnnotationEventHandlerInvokerTest {
     */
 
     @Test
-    public void testValidateEventHandler_MultiParamHandlerIsRejected() {
+    public void testValidateEventHandler_MultipleParameterHandlerIsRejected() {
         FirstSubclass handler = new IllegalEventHandler();
         try {
             AnnotationEventHandlerInvoker.validateHandlerMethods(handler);
