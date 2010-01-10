c61b9793d0b54f021e5aa7334704e149432d4d72
buijze <buijze@e329d720-ea76-11de-8016-dd4ad4b6fd10>
2010-01-10 17:02:56 +0000

    Removed unused InterruptedException in throws clause

--- src/test/java/nl/gridshore/cqrs4j/DomainEventTest.java
+++ src/test/java/nl/gridshore/cqrs4j/DomainEventTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009. Gridshore
+ * Copyright (c) 2010. Gridshore
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ public class DomainEventTest {
     }
 
     @Test
-    public void testDomainEventEquality() throws InterruptedException {
+    public void testDomainEventEquality() {
         DateTimeUtils.setCurrentMillisFixed(System.currentTimeMillis());
         DomainEvent eventWithoutAggrIdOrIdentifier = new StubDomainEvent();
         DomainEvent anotherEventWithoutAggrIdOrIdentifier = new StubDomainEvent();
